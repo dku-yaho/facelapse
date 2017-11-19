fae611e9d32b6dd44101b6d277d8649af54b2735
Hyebin Lee <hyeb2oo2@gmail.com>
2017-11-19 17:41:45 +0900

    Hyebin) git add?

---
+++ app/src/main/java/com/yaho/facelapse/ScreenActivity.java
@@ -0,0 +1,14 @@
+package com.yaho.facelapse;
+
+import  android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ScreenActivity extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+
+    }
+
+}

505315de6d1052d3c93b890ef435566925136f68
chaiwee <chaiweetan@gmail.com>
2017-12-07 14:47:52 +0900

    jobdispatcher done, need condition

--- app/src/main/java/com/yaho/facelapse/MainActivity.java
+++ app/src/main/java/com/yaho/facelapse/MainActivity.java
@@ -3,8 +3,6 @@ package com.yaho.facelapse;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.net.ConnectivityManager;
-import android.net.Uri;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,11 +11,22 @@ import android.view.Window;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
+
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     Validate validate;
-    boolean check;
+
+    private static final String JOB_TAG = "MyJobService";
+    FirebaseJobDispatcher mDispatcher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,20 +35,34 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        /* set font for main screen text*/
+        /* set font for title text*/
         TextView tv = (TextView) findViewById(R.id.FaceLapse);
         Typeface font = Typeface.createFromAsset(getAssets(), "AmaticSC-Bold.ttf");
         tv.setTypeface(font);
 
-        validate = new Validate(this);
+        /* set font for title text*/
+        TextView tv2 = (TextView) findViewById(R.id.textSelfie);
+        Typeface font2 = Typeface.createFromAsset(getAssets(), "AmaticSC-Bold.ttf");
+        tv2.setTypeface(font2);
 
-        if(!validate.isNetworkConnected()){
-            endActivity("Check your network");
-        }
-        if(!validate.validateTime()){
-            endActivity("Time Invalid. Adjust your local time");
+        /* set font for title text*/
+        TextView tv3 = (TextView) findViewById(R.id.textAlbum);
+        Typeface font3 = Typeface.createFromAsset(getAssets(), "AmaticSC-Bold.ttf");
+        tv3.setTypeface(font3);
+
+        validate = new Validate();
+        if(validate.validateTime() == false){
+            Toast.makeText(this, "Time Invalid", Toast.LENGTH_LONG).show();
+
+            new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                    MainActivity.this.finish();
+                }
+            }, 100);
         }
 
+        scheduleJob(this);
 
     }
 
@@ -53,8 +76,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.buttonalbum:
                 // ALBUM 버튼이 눌렸을 때
                 Toast.makeText(this, "ALBUM button clicked", Toast.LENGTH_LONG).show();
-                Uri uri = Uri.parse("content://media/internal/images/media");
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                Intent intent = new Intent(getApplicationContext(), ScreenActivity.class);
                 startActivity(intent);
                 break;
             case R.id.buttonmanual:
@@ -63,14 +85,33 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void endActivity(String str){
-        Toast.makeText(this, str, Toast.LENGTH_LONG).show();
 
-        new Handler().postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                MainActivity.this.finish();
-            }
-        }, 100);
+    private void scheduleJob(Context context) {
+        //creating new firebase job dispatcher
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+        //creating new job and adding it with dispatcher
+        Job myJob = mDispatcher.newJobBuilder()
+                .setService(MyJobService.class)
+                .setTag(JOB_TAG)
+                .setRecurring(true)
+                .setTrigger(Trigger.executionWindow(30, 60))
+                .setLifetime(Lifetime.UNTIL_NEXT_BOOT)
+                .setReplaceCurrent(false)
+                .setConstraints(Constraint.ON_ANY_NETWORK)
+                .setRetryStrategy(RetryStrategy.DEFAULT_LINEAR)
+                .build();
+        mDispatcher.mustSchedule(myJob);
+        //Toast.makeText(this, R.string.job_scheduled, Toast.LENGTH_LONG).show();
     }
+
+    private void cancelJob(String jobTag) {
+        if ("".equals(jobTag)) {
+            mDispatcher.cancelAll();
+        } else {
+            mDispatcher.cancel(jobTag);
+        }
+       // Toast.makeText(this, R.string.job_cancelled, Toast.LENGTH_LONG).show();
+    }
+
+
 }
--- app/src/main/java/com/yaho/facelapse/MyJobService.java
+++ app/src/main/java/com/yaho/facelapse/MyJobService.java
@@ -4,23 +4,29 @@ import android.app.job.JobService;
 import android.util.Log;
 import com.firebase.jobdispatcher.JobParameters;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 /**
  * Created by qoren on 2017-12-05.
  */
 
-public class MyJobService extends com.firebase.jobdispatcher.JobService {
+public class MyJobService extends com.firebase.jobdispatcher.JobService{
 
     private static final String TAG = "MyJobService";
 
     @Override
-    public boolean onStartJob(JobParameters jobParameters) {
+    public boolean onStartJob (JobParameters jobParameters){
+        String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        Log.d(TAG, currentDateTimeString);
         Log.d(TAG, "Performing long running task in scheduled job");
-        // TODO(developer): add long running task here.
         return false;
     }
 
     @Override
-    public boolean onStopJob(JobParameters jobParameters) {
+    public boolean onStopJob (JobParameters jobParameters){
+        Log.d(TAG, "Job cancelled!");
         return false;
     }
+
 }
